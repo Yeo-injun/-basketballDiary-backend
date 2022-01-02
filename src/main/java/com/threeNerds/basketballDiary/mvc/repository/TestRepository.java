cb85c967a69f0c0082d559cd93b4e49768d21b20
Devy <youth.com11@gmail.com>
2022-01-03 00:39:17 +0900

    commit

--- src/main/java/com/threeNerds/basketballDiary/mvc/repository/TestRepository.java
+++ src/main/java/com/threeNerds/basketballDiary/mvc/repository/TestRepository.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 @Mapper // Mapper 인터페이스를 사용하면 별도의 java파일을 생성하지 않고 바로 xml로 접근가능!
+
 public interface TestRepository {
 
     TestDTO testSelect();
