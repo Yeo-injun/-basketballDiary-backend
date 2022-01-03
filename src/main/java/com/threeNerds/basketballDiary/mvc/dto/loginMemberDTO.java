6f43aab25452af7be0325c557ce24e303d3fde68
seongju lee <gch03915@naver.com>
2022-01-03 22:04:33 +0900

    first commit : +domain(Address,Member) , +dto(loginMemberDTO)

---
+++ src/main/java/com/threeNerds/basketballDiary/mvc/dto/loginMemberDTO.java
@@ -0,0 +1,12 @@
+package com.threeNerds.basketballDiary.mvc.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class loginMemberDTO {
+
+    private String memberId;
+
+    private String password;
+}
