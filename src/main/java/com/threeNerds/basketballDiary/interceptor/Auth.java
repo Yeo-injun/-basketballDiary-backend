307edffda6f3dc61b9f3f99e5bae362ad3679c72
seongju lee <gch03915@naver.com>
2022-01-24 23:46:24 +0900

    complete Auth + Login AND +logout

--- src/main/java/com/threeNerds/basketballDiary/interceptor/Auth.java
+++ src/main/java/com/threeNerds/basketballDiary/interceptor/Auth.java
@@ -8,5 +8,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Auth {
-    int GRADE() default 0;
+    long GRADE() default 0;
 }
