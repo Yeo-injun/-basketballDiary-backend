88a637515d7a2e30447759894b088a693cec691d
Devy <youth.com11@gmail.com>
2022-02-15 23:55:48 +0900

    Implementing API of invite team member.

--- src/main/java/com/threeNerds/basketballDiary/mvc/service/TeamMemberManagerService.java
+++ src/main/java/com/threeNerds/basketballDiary/mvc/service/TeamMemberManagerService.java
@@ -1,5 +1,9 @@
 package com.threeNerds.basketballDiary.mvc.service;
 
+import com.threeNerds.basketballDiary.mvc.domain.JoinRequestStateCode;
+import com.threeNerds.basketballDiary.mvc.domain.JoinRequestTypeCode;
+import com.threeNerds.basketballDiary.mvc.domain.TeamJoinRequest;
+import com.threeNerds.basketballDiary.mvc.repository.TeamJoinRequestRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -20,4 +24,31 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional
 public class TeamMemberManagerService {
+
+    private final TeamJoinRequestRepository teamJoinRequestRepository;
+
+    // 팀원 초대 API
+    public void inviteTeamMember(TeamJoinRequest teamJoinRequest) {
+        TeamJoinRequest invitationInfo = TeamJoinRequest.builder()
+                .teamSeq(teamJoinRequest.getTeamSeq())
+                .userSeq(teamJoinRequest.getUserSeq())
+                .joinRequestTypeCode(JoinRequestTypeCode.INVITATION.getCode())
+                .joinRequestStateCode(JoinRequestStateCode.WAITING.getCode())
+                .build();
+
+        // 초대-가입요청 존재여부 확인 : 대기중인 가입요청 혹은 초대가 있을 경우 중복가입요청 방지
+        TeamJoinRequest prevJoinReq = null; // = teamJoinRequestRepository.checkJoinRequest(invitationInfo);
+        boolean isExistJoinRequest = prevJoinReq != null
+                                        ? true
+                                        : false;
+        if (isExistJoinRequest)
+        {
+            log.debug("이미 {}이 있습니다.", JoinRequestTypeCode.valueOf(prevJoinReq.getJoinRequestTypeCode()));
+            return; // TODO 에러를 던지거나 다른 것을 리턴하기
+        }
+        teamJoinRequestRepository.createJoinRequest(invitationInfo);
+    }
+
+
+
 }
