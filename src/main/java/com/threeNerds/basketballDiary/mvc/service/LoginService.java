db9af672b2fd2b663ea3d30dddab584db4d9fa0f
Devy <67055084+Yeo-injun@users.noreply.github.com>
2022-03-22 22:44:56 +0900

    [FIX] Login API 호출시 권한 목록 조회 쿼리 수정(LEFT JOIN에서 INNER JOIN으로 : LEFT JOIN의 경우 TEAM_MEMBER테이블에 데이터 존재여부와 상관없이 USER테이블에 사용자의 데이터가 있기 때문에 null인 레코드가 반환되는 이슈였음.)

--- src/main/java/com/threeNerds/basketballDiary/mvc/service/LoginService.java
+++ src/main/java/com/threeNerds/basketballDiary/mvc/service/LoginService.java
@@ -35,15 +35,10 @@ public class LoginService {
         //null 인지 null이 아닌지 확신이 들지 않을때 사용 : ofNullable(null 이면 Optional.empty() 반환)
         SessionUser sessionUser = Optional.ofNullable(findLoginUser)
                                     .map(loginUser -> {
-                                        List<AuthUserRequestDTO> userAuthList = userRepository.findAuthList(cmnUserDTO);
+                                        List<AuthUserRequestDTO> userAuthList = userRepository.findAuthList(findLoginUser);
 
                                         /** 팀에 소속되어 있지 않을 경우 - 권한정보없이 SessionUser객체 생성 */
-                                        // TODO 왜 list에 값이 없는데 size가 왜 1인지 확인 필요... >
-                                        // Mybatis의 기본 설정이 조회 레코드가 없으면 null을 반환하게 됨.
-                                        // 이때문에 List안에 첫번째 요소로 null이 들어가 있는 상태가 됨.
-                                        // 추가적으로 레코드가 없을때 return되는 값을 null이 아닌 인스턴스로 할 수 있음
-                                        // 하지만 이경우에도 조회된 값이 없기때문에 return되는 인스턴스 안에 필드는 모두 null이 됨.
-                                        if (userAuthList.get(0) == null && userAuthList.size() == 1)
+                                        if (userAuthList.isEmpty())
                                         {
                                             return new SessionUser(loginUser.getUserSeq(), loginUser.getUserId());
                                         }
