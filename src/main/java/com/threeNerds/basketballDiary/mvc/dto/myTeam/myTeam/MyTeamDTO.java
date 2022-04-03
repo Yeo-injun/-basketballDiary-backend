09f21df62e6a6b042ae03c33e7c8607d05607905
강창기 <elien940318@gmail.com>
2022-04-03 14:40:37 +0900

    4월1일 안건내역 반영(ResponseBody JSON 구조 변경), BeanUtils.copyProperties -> method chaining으로 변경, Controller 권한 부여

--- src/main/java/com/threeNerds/basketballDiary/mvc/dto/myTeam/myTeam/MyTeamDTO.java
+++ src/main/java/com/threeNerds/basketballDiary/mvc/dto/myTeam/myTeam/MyTeamDTO.java
@@ -8,14 +8,79 @@ import java.util.List;
 @Getter
 public class MyTeamDTO {
 
-    /* 소속팀 정보 */
-    private MyTeamInfoDTO  myTeamInfo;
+    /**
+     * 소속팀 단건 정보
+     */
+    // TEAM
+    /* 팀 pk */
+    private Long teamSeq;
+    /* 팀명 */
+    private String teamName;
+    /* 팀 이미지 경로 */
+    private String teamImagePath;
+    /* 연고지 */
+    private String hometown;
+    /* 시도코드 */
+    private String sidoCode;
+    /* 시군구코드 */
+    private String sigunguCode;
+    /* 창단일 */
+    private String foundationYmd;
+    /* 팀 소개 */
+    private String introduction;
 
-    /* 정기운동 목록 */
+    // TEAM_MEMBER
+    /* 회원수 */
+    private Integer totMember;
+
+    /**
+     * 정기운동 목록 정보
+     */
     private List<TeamRegularExercise> teamRegularExercisesList;
 
-    public MyTeamDTO myTeamInfo (MyTeamInfoDTO myTeamInfo) {
-        this.myTeamInfo = myTeamInfo;
+
+    public MyTeamDTO teamSeq (Long teamSeq) {
+        this.teamSeq = teamSeq;
+        return this;
+    }
+
+    public MyTeamDTO teamName (String teamName) {
+        this.teamName = teamName;
+        return this;
+    }
+
+    public MyTeamDTO teamImagePath (String teamImagePath) {
+        this.teamImagePath = teamImagePath;
+        return this;
+    }
+
+    public MyTeamDTO hometown (String hometown) {
+        this.hometown = hometown;
+        return this;
+    }
+
+    public MyTeamDTO sidoCode (String sidoCode) {
+        this.sidoCode = sidoCode;
+        return this;
+    }
+
+    public MyTeamDTO sigunguCode (String sigunguCode) {
+        this.sigunguCode = sigunguCode;
+        return this;
+    }
+
+    public MyTeamDTO foundationYmd (String foundationYmd) {
+        this.foundationYmd = foundationYmd;
+        return this;
+    }
+
+    public MyTeamDTO introduction (String introduction) {
+        this.introduction = introduction;
+        return this;
+    }
+
+    public MyTeamDTO totMember (Integer totMember) {
+        this.totMember = totMember;
         return this;
     }
 
--- src/main/java/com/threeNerds/basketballDiary/mvc/repository/TeamRepository.java
+++ src/main/java/com/threeNerds/basketballDiary/mvc/repository/TeamRepository.java
@@ -2,7 +2,7 @@ package com.threeNerds.basketballDiary.mvc.repository;
 
 import com.threeNerds.basketballDiary.mvc.domain.Team;
 import com.threeNerds.basketballDiary.mvc.dto.team.team.SearchTeamDTO;
-import com.threeNerds.basketballDiary.mvc.dto.team.team.TeamInfoDTO;
+import com.threeNerds.basketballDiary.mvc.dto.team.team.TeamDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -12,7 +12,7 @@ public interface TeamRepository {
     Long saveTeam(Team team);
     void updateTeam(Team team);
     Team findByTeamSeq(Long teamSeq);
-    List<TeamInfoDTO> findAll(SearchTeamDTO searchTeamDTO);
-    List<TeamInfoDTO> findPagingTeam(SearchTeamDTO searchTeamDTO);
+    List<TeamDTO> findAll(SearchTeamDTO searchTeamDTO);
+    List<TeamDTO> findPagingTeam(SearchTeamDTO searchTeamDTO);
     void deleteById(Long teamSeq);
 }
