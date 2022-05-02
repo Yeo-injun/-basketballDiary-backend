8590855f20af8b2ea0bea3713f16006f300518ce
Devy <67055084+Yeo-injun@users.noreply.github.com>
2022-05-02 23:08:05 +0900

    [UPDATE] WebConfig CORS 설정 변경(브라우저에서 모든 Http header 대해서 접근가능)

--- src/main/java/com/threeNerds/basketballDiary/config/WebConfig.java
+++ src/main/java/com/threeNerds/basketballDiary/config/WebConfig.java
@@ -21,7 +21,11 @@ public class WebConfig implements WebMvcConfigurer {
         WebMvcConfigurer.super.addCorsMappings(registry);
         registry.addMapping("/api/**")                      // /api 하위 API call 허용
                 .allowedOrigins("*")
-                .allowedMethods("*");
+                .allowedMethods("*")
+                // CORS은 기본헤더만 접근가능하게 함. 따라서 브라우저에서는 set-Cookie 헤더에 접근이 불가함. 접근을 허용하기 위해 설정을 추가
+                // https://bogmong.tistory.com/5
+                .allowedHeaders("*");
+
                 //.allowedOrigins("http://127.0.0.1:5500")    // 로컬 프론트에서만 허용
                 //.allowedMethods("GET");                     // GET Method에 대해서만 허용
     }
