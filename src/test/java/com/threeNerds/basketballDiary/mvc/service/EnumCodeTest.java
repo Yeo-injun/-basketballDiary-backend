8764812c9d6393c7806ddc5bbf7b913016ec7483
Devy <67055084+Yeo-injun@users.noreply.github.com>
2022-08-18 22:21:28 +0900

    [Refactor] Enum코드 클래스에서 구현한 nameOf메소드 테스트 코드 추가

---
+++ src/test/java/com/threeNerds/basketballDiary/mvc/service/EnumCodeTest.java
@@ -0,0 +1,19 @@
+package com.threeNerds.basketballDiary.mvc.service;
+
+import com.threeNerds.basketballDiary.constant.JoinRequestTypeCode;
+import org.junit.jupiter.api.Test;
+
+
+public class EnumCodeTest {
+    @Test
+    void 코드값으로_ENUM에서_코드이름추출하기() {
+        String codeName = JoinRequestTypeCode.nameOf("01");
+        System.out.println(codeName);
+    }
+
+    @Test
+    void 코드값으로_ENUM에서_코드이름추출하기_code값이_없는_경우() {
+        String codeName = JoinRequestTypeCode.nameOf("03");
+        System.out.println(codeName);
+    }
+}
