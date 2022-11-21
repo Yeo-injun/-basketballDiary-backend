a479081821e9ab02d3251efcaad5a97610adc4be
changgi <elien940318@gmail.com>
2022-11-21 23:21:27 +0900

    [feat] API038 쿼터 저장/수정하기 DTO, 조회쿼리 작성

--- src/main/java/com/threeNerds/basketballDiary/mvc/game/dto/PlayerRecordDTO.java
+++ src/main/java/com/threeNerds/basketballDiary/mvc/game/dto/PlayerRecordDTO.java
@@ -4,15 +4,40 @@ import lombok.Getter;
 
 @Getter
 public class PlayerRecordDTO {
+
+    /**
+     * GAME_JOIN_TEAM
+     */
+    /** 게임참여팀 SEQ */
+    private Long gameJoinTeamSeq;
+    private String homeAwayCode;
+    private String gameSeq;
+    private String teamSeq;
+
+    /**
+     * GAME_JOIN_PLAYER
+     */
+    /** 유저 SEQ */
     private Long userSeq;
     private String name;
     private String backNumber;
     private String positionCode;
+
     private String userImage;
+
+    /**
+     * QUARTER_PLAYER_RECORDS
+     */
+    /** 쿼터별선수기록 SEQ */
+    private Long quarterPlayerRecordsSeq;
+    /** 쿼터코드; 01~04(1~4쿼터), 11(전반), 12(후반) */
     private String quarterCode;
     private Long freeThrow;
+    private Long tryFreeThrow;
     private Long twoPoint;
+    private Long tryTwoPoint;
     private Long threePoint;
+    private Long tryThreePoint;
     private Long totalScore;
     private Long assist;
     private Long rebound;
@@ -20,6 +45,27 @@ public class PlayerRecordDTO {
     private Long block;
     private Long turnover;
     private Long foul;
+
+
+    public PlayerRecordDTO gameJoinTeamSeq(Long gameJoinTeamSeq) {
+        this.gameJoinTeamSeq = gameJoinTeamSeq;
+        return this;
+    }
+
+    public PlayerRecordDTO homeAwayCode(String homeAwayCode) {
+        this.homeAwayCode = homeAwayCode;
+        return this;
+    }
+
+    public PlayerRecordDTO gameSeq(String gameSeq) {
+        this.gameSeq = gameSeq;
+        return this;
+    }
+
+    public PlayerRecordDTO teamSeq(String teamSeq) {
+        this.teamSeq = teamSeq;
+        return this;
+    }
 
     public PlayerRecordDTO userSeq(Long userSeq) {
         this.userSeq = userSeq;
@@ -41,6 +87,11 @@ public class PlayerRecordDTO {
         this.userImage = userImage;
         return this;
     }
+
+    public PlayerRecordDTO quarterPlayerRecordsSeq(Long quarterPlayerRecordsSeq) {
+        this.quarterPlayerRecordsSeq = quarterPlayerRecordsSeq;
+        return this;
+    }
     public PlayerRecordDTO quarterCode(String quarterCode) {
         this.quarterCode = quarterCode;
         return this;
@@ -50,12 +101,24 @@ public class PlayerRecordDTO {
         this.freeThrow = freeThrow;
         return this;
     }
+    public PlayerRecordDTO tryFreeThrow(Long tryFreeThrow) {
+        this.tryFreeThrow = tryFreeThrow;
+        return this;
+    }
     public PlayerRecordDTO twoPoint(Long twoPoint) {
         this.twoPoint = twoPoint;
         return this;
     }
+    public PlayerRecordDTO tryTwoPoint(Long tryTwoPoint) {
+        this.tryTwoPoint = tryTwoPoint;
+        return this;
+    }
     public PlayerRecordDTO threePoint(Long threePoint) {
         this.threePoint = threePoint;
+        return this;
+    }
+    public PlayerRecordDTO tryThreePoint(Long tryThreePoint) {
+        this.tryThreePoint = tryThreePoint;
         return this;
     }
     public PlayerRecordDTO totalScore(Long totalScore) {
