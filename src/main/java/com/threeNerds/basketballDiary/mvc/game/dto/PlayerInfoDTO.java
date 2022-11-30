0762488637ba39d15a0488a1805d393c9cbeb6aa
seongju lee <gch03915@naver.com>
2022-11-30 22:45:19 +0900

    20221130 : API061 개발중

---
+++ src/main/java/com/threeNerds/basketballDiary/mvc/game/dto/PlayerInfoDTO.java
@@ -0,0 +1,19 @@
+package com.threeNerds.basketballDiary.mvc.game.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PlayerInfoDTO {
+
+    private Long userSeq;
+
+    private String userName;
+
+    private String email;
+
+    private String positionCode;
+
+    private String backNumber;
+
+    private String playerTypeCode;
+}
