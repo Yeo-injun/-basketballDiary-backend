0fd9cc60d020aca1850340a7f89ac814bfebcbe7
Devy <67055084+Yeo-injun@users.noreply.github.com>
2023-11-23 22:52:44 +0900

    [ Chore ] 이미지 조회시 발생하는 FileNotFoundException 처리방법 고민중...

--- src/main/java/com/threeNerds/basketballDiary/file/ImageProvider.java
+++ src/main/java/com/threeNerds/basketballDiary/file/ImageProvider.java
@@ -1,6 +1,7 @@
 package com.threeNerds.basketballDiary.file;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
@@ -22,6 +23,7 @@ import java.net.MalformedURLException;
  *          참고자료 : https://velog.io/@gmtmoney2357/%EC%8A%A4%ED%94%84%EB%A7%81-%EB%B6%80%ED%8A%B8-%ED%8C%8C%EC%9D%BC-%EC%97%85%EB%A1%9C%EB%93%9C-%EB%8B%A4%EC%9A%B4%EB%A1%9C%EB%93%9C-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EB%B3%B4%EC%97%AC%EC%A3%BC%EA%B8%B0
  *
  */
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ImageProvider implements Provider {
@@ -30,10 +32,16 @@ public class ImageProvider implements Provider {
 
     @Override
     public Resource provide( String saveUrl ) throws MalformedURLException {
-        // TODO 작동방식 확인하기
-        // 로컬 테스트 결과 - 성공
-        // 개발서버 테스트 결과 -
-        return new FileSystemResource( imagePathManager.toPath( saveUrl, PathManager.Type.IMAGE ) );
+        // TODO 작동방식 확인하기 - 파일이 존재하지 않을 경우 발생하는 에러로그 처리
+        /**
+         * 20231123 22:50:01.190 [http-nio-18080-exec-6] ERROR o.a.c.c.C.[.[.[.[dispatcherServlet] - Servlet.service() for servlet [dispatcherServlet] in context with path [] threw exception
+         * java.io.FileNotFoundException: file [D:\image\myTeams\info\20231102\1111.jpg] cannot be resolved in the file system for checking its content length
+         * 	at org.springframework.core.io.FileSystemResource.contentLength(FileSystemResource.java:279)
+         */
+        log.info( "IMAGE SAVE URL : {}", saveUrl );
+        Resource imageResource = new FileSystemResource( imagePathManager.toPath( saveUrl, PathManager.Type.IMAGE ) );
+        log.info( "Success Get SAVE Image : {}", saveUrl );
+        return imageResource;
     }
 
 }
