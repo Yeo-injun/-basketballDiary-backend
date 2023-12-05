fe587086c8ce297577af92a6b079ec0ee1820663
Devy <youth.com11@gmail.com>
2023-12-06 00:07:06 +0900

    [ Chore ] 미사용 import문 제거

--- src/test/java/com/threeNerds/basketballDiary/mvc/auth/AuthApiTest.java
+++ src/test/java/com/threeNerds/basketballDiary/mvc/auth/AuthApiTest.java
@@ -1,14 +1,12 @@
 package com.threeNerds.basketballDiary.mvc.auth;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.threeNerds.basketballDiary.exception.error.BindErrorType;
 import com.threeNerds.basketballDiary.mvc.auth.controller.request.CheckDuplicateUserIdRequest;
 import com.threeNerds.basketballDiary.mvc.auth.controller.request.CreateUserRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
