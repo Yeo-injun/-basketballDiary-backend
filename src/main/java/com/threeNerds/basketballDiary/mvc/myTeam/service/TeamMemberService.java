10bbca5e5e8f59f43a62fffba233f0224ae8a9db
Devy <youth.com11@gmail.com>
2024-02-27 00:15:47 +0900

    [ Chore ] 팀원에 대한 서비스 로직 재정비

--- src/main/java/com/threeNerds/basketballDiary/mvc/myTeam/service/TeamMemberService.java
+++ src/main/java/com/threeNerds/basketballDiary/mvc/myTeam/service/TeamMemberService.java
@@ -13,7 +13,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
+
 
 /**
  *  Service
