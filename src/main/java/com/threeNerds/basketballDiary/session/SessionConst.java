681b47492392b1c2e8f5087163309c7b12a923af
Devy <youth.com11@gmail.com>
2024-02-27 22:36:37 +0900

    [ Chore ] 미사용 상수 클래스 제거

--- src/main/java/com/threeNerds/basketballDiary/session/SessionConst.java
+++
@@ -1,6 +0,0 @@
-package com.threeNerds.basketballDiary.session;
-
-public class SessionConst {
-    public static final String USER_SESSION = "userSession";
-    public static final String AUTH_LIST = "authList";
-}
