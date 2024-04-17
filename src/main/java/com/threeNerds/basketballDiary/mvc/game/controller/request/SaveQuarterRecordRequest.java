a32347e8ac521d69803d3da394ec1e1b6bcc3627
Devy <youth.com11@gmail.com>
2024-04-18 02:02:42 +0900

    [ Fix ] API038 경기 쿼터기록 저장 - 쿼터기록 저장시 출전선수 기록기준으로 팀 기록을 합산하였던 오류 해결.
    - 해결방식
    1. 쿼터 기록 저장시 팀 출전선수 기록을 먼저 저장
    2. 쿼터의 모든 선수기록을 조회하여 팀별 쿼터 기록 합산하여 저장
       ( 출전하지 않은 선수들의 기록도 같이 팀의 쿼터기록으로 합산 처리 )

---
+++ src/main/java/com/threeNerds/basketballDiary/mvc/game/controller/request/SaveQuarterRecordRequest.java
@@ -0,0 +1,35 @@
+package com.threeNerds.basketballDiary.mvc.game.controller.request;
+
+import com.threeNerds.basketballDiary.mvc.game.dto.saveQuarterRecords.request.SavePlayerRecordDTO;
+import com.threeNerds.basketballDiary.mvc.game.service.dto.QuarterRecordCommand;
+import com.threeNerds.basketballDiary.mvc.user.service.dto.MembershipCommand;
+import lombok.Getter;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+
+@Getter
+public class SaveQuarterRecordRequest {
+
+    private Long gameSeq;
+    private String quarterCode;
+
+    @NotEmpty(message = "쿼터 시간은 필수 입력항목입니다.")
+    private String quarterTime;
+//    @NotEmpty(message = "홈팀 선수들의 경기기록 정보가 없습니다.") // TODO 향후 홈/어웨이 팀만 저장할 수 있도록 처리 검토
+    private List<SavePlayerRecordDTO> homeTeamPlayerRecords;
+//    @NotEmpty(message = "어웨이팀 선수들의 경기기록 정보가 없습니다.") // TODO 향후 홈/어웨이 팀만 저장할 수 있도록 처리 검토
+    private List<SavePlayerRecordDTO> awayTeamPlayerRecords;
+
+    public QuarterRecordCommand toCommand( Long gameSeq, String quarterCode ) {
+        return QuarterRecordCommand.builder()
+                .gameSeq(       gameSeq )
+                .quarterCode(   quarterCode )
+                .quarterTime(   this.quarterTime )
+                .homeTeamPlayerRecords( this.homeTeamPlayerRecords )
+                .awayTeamPlayerRecords( this.awayTeamPlayerRecords )
+                .build();
+    }
+}
