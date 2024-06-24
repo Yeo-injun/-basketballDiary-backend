8967d08aa417fa72b025c232a58320a367fabc09
Devy <youth.com11@gmail.com>
2024-06-24 09:43:49 +0900

    [ Refactor ] API043 경기 전체참가선수 조회 Query - Result패턴 및 ApiDocs 적용

--- src/main/java/com/threeNerds/basketballDiary/mvc/game/service/dto/GameJoinPlayerRecordQuery.java
+++ src/main/java/com/threeNerds/basketballDiary/mvc/game/service/dto/GameJoinPlayerRecordQuery.java
@@ -1,9 +1,12 @@
 package com.threeNerds.basketballDiary.mvc.game.service.dto;
 
+import com.threeNerds.basketballDiary.mvc.game.dto.PlayerQuarterRecordDTO;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Getter
 @Builder
@@ -15,4 +18,29 @@ public class GameJoinPlayerRecordQuery {
     private String quarterCode;
     private String homeAwayCode;
 
+    @Getter
+    public class Result {
+        private final Long gameSeq;
+        private final String quarterCode;
+        private final List<PlayerQuarterRecordDTO> homeTeamPlayers;
+        private final List<PlayerQuarterRecordDTO> awayTeamPlayers;
+
+        Result(
+            GameJoinPlayerRecordQuery query,
+            List<PlayerQuarterRecordDTO> homeTeamPlayers,
+            List<PlayerQuarterRecordDTO> awayTeamPlayers
+        ) {
+            this.gameSeq            = query.getGameSeq();
+            this.quarterCode        = query.getQuarterCode();
+            this.homeTeamPlayers    = homeTeamPlayers;
+            this.awayTeamPlayers    = awayTeamPlayers;
+        }
+    }
+
+    public Result buildResult(
+        List<PlayerQuarterRecordDTO> homeTeamPlayers,
+        List<PlayerQuarterRecordDTO> awayTeamPlayers
+    ) {
+        return new Result( this, homeTeamPlayers, awayTeamPlayers );
+    }
 }
---
+++ src/main/java/com/threeNerds/basketballDiary/swagger/docs/game/ApiDocs043.java
@@ -0,0 +1,36 @@
+package com.threeNerds.basketballDiary.swagger.docs.game;
+
+import com.threeNerds.basketballDiary.exception.error.DomainErrorResponse;
+import com.threeNerds.basketballDiary.exception.error.DomainErrorType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.MediaType;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+
+@Target( METHOD )
+@Retention( RetentionPolicy.RUNTIME )
+@Operation(
+    summary     = "[ API043 ] 쿼터별 경기참가선수들의 경기기록 조회",
+    description = "경기 쿼터별로 경기에 참가한 모든 선수들의 경기기록을 조회한다."
+                + "[ 제약사항 ] 1. 페이징 처리가 없다. 2. Home/Away팀 모두 한번에 조회한다.",
+    responses   = {
+        @ApiResponse(
+            responseCode    = "200",
+            description     = "쿼터별 경기참가선수들의 경기기록을 조회했습니다.",
+            content         = @Content( mediaType = MediaType.APPLICATION_JSON_VALUE )
+        ),
+        @ApiResponse(
+            responseCode    = "404",
+            description     = "경기 정보가 존재하지 않습니다.",
+            content         = @Content( schema = @Schema( implementation = DomainErrorResponse.class ) )
+        ),
+    }
+)
+public @interface ApiDocs043 { }
