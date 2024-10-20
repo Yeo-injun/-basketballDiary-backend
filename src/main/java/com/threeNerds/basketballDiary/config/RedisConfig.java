536db2e734af8c607a6c84df2986489538cceeb7
Devy <youth.com11@gmail.com>
2024-10-20 20:42:47 +0900

    [ Feat ] Session 저장소를 Redis로 변경

---
+++ src/main/java/com/threeNerds/basketballDiary/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.threeNerds.basketballDiary.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800)  // 세션 유효 시간 설정 (30분)
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(   redisHost );
+        redisStandaloneConfiguration.setPort(       redisPort );
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+}
