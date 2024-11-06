035dda7ef6e6500eb8d2ea55ce10f24ec0cc868c
Devy <youth.com11@gmail.com>
2024-11-07 00:06:31 +0900

    [ Fix ] 경기기록원 저장 API - 권한부여 요청을 받은 자가 이미 경기기록권한을 가지고 있는지 확인하는 로직 보완

--- src/main/java/com/threeNerds/basketballDiary/exception/error/DomainErrorType.java
+++ src/main/java/com/threeNerds/basketballDiary/exception/error/DomainErrorType.java
@@ -53,6 +53,7 @@ public enum DomainErrorType implements ErrorMessageType {
     CANT_DECISION_JOIN_REQUEST_BY_TEAM( FORBIDDEN, "팀이 처리할 수 있는 가입요청이 아닙니다."),
     INSUFFICIENT_GAME_JOIN_TEAMS( FORBIDDEN, "경기에 참가하는 팀의 수가 부족합니다."),
     ALREADY_EXIST_QUARTER_RECORDS(FORBIDDEN, "이미 쿼터기록이 입력되고 있어 선수 추가가 불가합니다."),
+    ALREADY_EXIST_RECORD_AUTH(FORBIDDEN, "이미 경기기록권한을 가지고 있습니다."),
 
 
     /** 409 CONFLICT : Resource 의 현재 상태와 충돌. 보통 중복된 데이터 존재 */
