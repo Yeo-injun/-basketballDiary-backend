718d69b0331e8d8826031c8477f42e2624aaef14
Devy <youth.com11@gmail.com>
2024-12-07 23:14:13 +0900

    [ Chore ] 미사용 패키지 정리 http

--- src/main/java/com/threeNerds/basketballDiary/http/ResponseJsonBody.java
+++
@@ -1,8 +0,0 @@
-package com.threeNerds.basketballDiary.http;
-
-public class ResponseJsonBody implements IResponseBody {
-
-    public <T> T cast( Object targetObj ) {
-        return (T) targetObj;
-    }
-}
